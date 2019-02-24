93158d32ee45206f399acabb7673bbeb11ee7942
Gareth Slinn <garethslinn@gareths-mbp.home>
2019-02-24 20:19:15 +0000

    apply example page with joke function

---
+++ home/src/App.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import Joke from './Joke';
+
+function App() {
+  const [userQuery, setUserQuery] = useState('');
+
+  const updateUserQuery = event => {
+    setUserQuery(event.target.value)
+  };
+
+  const searchQuery = () => {
+    window.open(`https://google.com/search?q=${userQuery}`, '_blank');
+  };
+
+  const handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      searchQuery();
+    }
+  };
+
+    return (
+      <div className="App">
+            <h1>Search</h1>
+        <div className="form">
+          <input value={userQuery} onChange={updateUserQuery} onKeyPress={handleKeyPress}/>
+          <button onClick={searchQuery}>Search</button>
+        </div>
+        <hr />
+        <Joke />
+      </div>
+    );
+}
+
+export default App;
