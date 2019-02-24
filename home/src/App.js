a392b7fb4ce4a9d0c33de20d7efda8609cd1c5d2
Gareth Slinn <garethslinn@gareths-mbp.home>
2019-02-24 20:44:23 +0000

    apply stories

--- home/src/App.js
+++ home/src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import Joke from './Joke';
+import Stories from './Stories';
 
 function App() {
   const [userQuery, setUserQuery] = useState('');
@@ -27,6 +28,8 @@ function App() {
         </div>
         <hr />
         <Joke />
+        <hr />
+        <Stories />
       </div>
     );
 }
